23d990c7401de2581211277dd5195adeea4fbd58
user <doma06@vse.cz>
2024-10-06 09:47:00 +0200

    vytvoření controlleru a vypsání vstupu do výstupu

---
+++ src/main/java/com/example/hellokittyadventura/main/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.hellokittyadventura.main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HomeController {
+
+    @FXML
+    private TextArea vystup;
+    @FXML
+    private TextField vstup;
+
+    @FXML
+    private void odesliVstup(ActionEvent actionEvent) {
+        vystup.appendText(vstup.getText()+"\n");
+        vstup.clear();
+    }
+}
